da4ac03b591455cfbe3f2910d8a86244f4f2e9d6
Leonard Fischer <lfischer@i-doit.com>
2019-03-08 10:47:19 +0100

    Updating the API client to work with different methods - also fixing a bug that made it unusable (API response changed?)

--- src/API.php
+++ src/API.php
@@ -9,53 +9,50 @@ namespace lfischer\openWeatherMap;
  */
 class API extends Request
 {
-	/**
-	 * Retrieves weather data by a (API internal) "city ID".
-	 *
-	 * @param integer $id
-	 * @return array
-	 */
-	public function getByCityId ($id)
-	{
-		return $this->fetch(['id' => $id])->getResponseArray();
-	} // function
+    /**
+     * Retrieves weather data by a (API internal) "city ID".
+     *
+     * @param integer $id
+     * @return array
+     */
+    public function getByCityId($id)
+    {
+        return $this->fetch(['id' => $id])->getResponseArray();
+    }
 
+    /**
+     * Retrieves weather data by geo coordinates.
+     *
+     * @param float $lat
+     * @param float $lng
+     * @return array
+     */
+    public function getByCoordinates($lat, $lng)
+    {
+        return $this->fetch(['lat' => $lat, 'lon' => $lng])->getResponseArray();
+    }
 
-	/**
-	 * Retrieves weather data by geo coordinates.
-	 *
-	 * @param float $lat
-	 * @param float $lng
-	 * @return array
-	 */
-	public function getByCoordinates ($lat, $lng)
-	{
-		return $this->fetch(['lat' => $lat, 'lon' => $lng])->getResponseArray();
-	} // function
-
-
-	/**
-	 * Retrieves weather data by ZIP code and (optional) country code.
-	 *
-	 * @param string $zip
-	 * @param string $countryCode
-	 * @return array
-	 */
-	public function getByZipCode ($zip, $countryCode = null)
-	{
-		return $this->fetch(['zip' => $zip . ($countryCode !== null ? ',' . $countryCode : '')])->getResponseArray();
-	} // function
-
-
-	/**
-	 * Retrieves weather data by city name and (optional) country code.
-	 *
-	 * @param string $city
-	 * @param string $countryCode
-	 * @return array
-	 */
-	public function getByCityName ($city, $countryCode = null)
-	{
-		return $this->fetch(['q' => $city . ($countryCode !== null ? ',' . $countryCode : '')])->getResponseArray();
-	} // function
-} // class
+    /**
+     * Retrieves weather data by ZIP code and (optional) country code.
+     *
+     * @param string $zip
+     * @param string $countryCode
+     * @return array
+     */
+    public function getByZipCode($zip, $countryCode = null)
+    {
+        return $this->fetch(['zip' => $zip . ($countryCode !== null ? ',' . $countryCode : '')])->getResponseArray();
+    }
+    
+    /**
+     * Retrieves weather data by city name and (optional) country code.
+     *
+     * @param string $city
+     * @param string $countryCode
+     * @return array
+     */
+    public function getByCityName($city, $countryCode = null)
+    {
+        return $this->fetch(['q' => $city . ($countryCode !== null ? ',' . $countryCode : '')])->getResponseArray();
+    }
+}
--- src/Request.php
+++ src/Request.php
@@ -9,167 +9,216 @@ namespace lfischer\openWeatherMap;
  */
 class Request
 {
-	const URL = 'http://api.openweathermap.org/data/2.5/weather';
+    const URL = 'http://api.openweathermap.org/data/2.5/';
 
-	/**
-	 * @var array
-	 */
-	protected $parameters = [];
+    /**
+     * Unit constants.
+     */
+    const UNIT_KELVIN = 'standard';
+    const UNIT_FAHRENHEIT = 'imperial';
+    const UNIT_CELSIUS = 'metric';
 
-	/**
-	 * @var string
-	 */
-	protected $responseJSON = null;
+    /**
+     * Weather APIs.
+     */
+    const API_CURRENT_WEATHER = 'weather';
+    const API_FORECAST_HOURLY = 'forecast';
+    const API_FORECAST_DAYLY = 'forecast/daily';
 
-	/**
-	 * @var array
-	 */
-	protected $responseArray = null;
+    /**
+     * @var string
+     */
+    private $api = null;
 
+    /**
+     * @var array
+     */
+    private $parameters = [];
 
-	/**
-	 * Constructor.
-	 *
-	 * @param $apiKey
-	 */
-	public function __construct ($apiKey)
-	{
-		$this->parameters['appid'] = $apiKey;
-	} // function
+    /**
+     * @var string
+     */
+    private $responseJSON = null;
 
+    /**
+     * @var array
+     */
+    private $responseArray = null;
 
-	/**
-	 * Set the accuracy.
-	 * Possible values are:
-	 * - Close results: "close"
-	 * - Accurate results: "accurate"
-	 *
-	 * @param string $accuracy
-	 * @return $this
-	 * @see http://openweathermap.org/current#accuracy
-	 */
-	public function setAccuracy ($accuracy)
-	{
-		$this->parameters['type'] = $accuracy;
+    /**
+     * Constructor.
+     *
+     * @param $apiKey
+     */
+    public function __construct($apiKey)
+    {
+        $this->parameters['appid'] = $apiKey;
+        $this->api = self::API_CURRENT_WEATHER;
+    }
 
-		return $this;
-	} // function
+    /**
+     * Set a different type of API (current weather data, forecast, ...).
+     *
+     * @param  string $api
+     * @return $this
+     * @throws \ErrorException
+     */
+    public function setApi($api)
+    {
+        $allowedApis = [
+            self::API_CURRENT_WEATHER,
+            self::API_FORECAST_HOURLY,
+            self::API_FORECAST_DAYLY
+        ];
 
+        if (!in_array($api, $allowedApis, true)) {
+            throw new \ErrorException('The Open Weather Map API only allows following APIs: ' . implode(', ', $allowedApis));
+        }
 
-	/**
-	 * Set the response unit.
-	 * Possible values are:
-	 * - Kelvin: "standard"
-	 * - Fahrenheit: "imperial"
-	 * - Celsius: "metric"
-	 *
-	 * @param string $units
-	 * @return $this
-	 * @see http://openweathermap.org/current#data
-	 */
-	public function setUnits ($units)
-	{
-		$this->parameters['units'] = $units;
+        $this->api = $api;
 
-		return $this;
-	} // function
+        return $this;
+    }
 
+    /**
+     * Set the accuracy.
+     * Possible values are:
+     * - Close results: "close"
+     * - Accurate results: "accurate"
+     *
+     * @param string $accuracy
+     * @return $this
+     * @see http://openweathermap.org/current#accuracy
+     */
+    public function setAccuracy($accuracy)
+    {
+        $this->parameters['type'] = $accuracy;
 
-	/**
-	 * Set the response language. This will only be applied to the "description" field.
-	 * Possible values are:
-	 * - English: "en"
-	 * - Russian: "ru"
-	 * - Italian: "it"
-	 * - Spanish: "es" (or "sp")
-	 * - Ukrainian: "uk" (or "ua")
-	 * - German: "de"
-	 * - Portuguese: "pt"
-	 * - Romanian: "ro"
-	 * - Polish: "pl"
-	 * - Finnish: "fi"
-	 * - Dutch: "nl"
-	 * - French: "fr"
-	 * - Bulgarian: "bg"
-	 * - Swedish: "sv" (or "se")
-	 * - Chinese Traditional: "zh_tw"
-	 * - Chinese Simplified: "zh" (or "zh_cn")
-	 * - Turkish: "tr"
-	 * - Croatian: "hr"
-	 * - Catalan: "ca"
-	 *
-	 * @param string $lang
-	 * @return $this
-	 * @see http://openweathermap.org/current#multi
-	 */
-	public function setLanguage ($lang)
-	{
-		$this->parameters['lang'] = $lang;
+        return $this;
+    }
 
-		return $this;
-	} // function
+    /**
+     * Set the response unit.
+     * Possible values are:
+     * - Kelvin: "standard"
+     * - Fahrenheit: "imperial"
+     * - Celsius: "metric"
+     *
+     * @param string $units
+     * @return $this
+     * @see http://openweathermap.org/current#data
+     */
+    public function setUnits($units)
+    {
+        $allowedUnits = [
+            self::UNIT_KELVIN,
+            self::UNIT_FAHRENHEIT,
+            self::UNIT_CELSIUS
+        ];
 
+        if (!in_array($units, $allowedUnits, true)) {
+            throw new \ErrorException('The Open Weather Map API only allows following Units: ' . implode(', ', $allowedUnits));
+        }
 
-	/**
-	 * API fetch method.
-	 *
-	 * @param array $query
-	 * @return $this
-	 * @throws \ErrorException
-	 */
-	public function fetch (array $query = [])
-	{
-		$this->responseJSON = file_get_contents(self::URL . '?' . http_build_query(($this->parameters + $query), null, '&'));
-		$this->responseArray = json_decode($this->responseJSON, true);
+        $this->parameters['units'] = $units;
 
-		if (!is_array($this->responseArray))
-		{
-			throw new \ErrorException('The Open Weather Map API response returned no valid JSON: ' . $this->responseJSON);
-		} // if
+        return $this;
+    }
 
-		if (!isset($this->responseArray['response']))
-		{
-			throw new \ErrorException('The Open Weather Map API response is not set or empty: ' . $this->responseJSON);
-		} // if
+    /**
+     * Set the response language. This will only be applied to the "description" field.
+     * Possible values are:
+     * - English: "en"
+     * - Russian: "ru"
+     * - Italian: "it"
+     * - Spanish: "es" (or "sp")
+     * - Ukrainian: "uk" (or "ua")
+     * - German: "de"
+     * - Portuguese: "pt"
+     * - Romanian: "ro"
+     * - Polish: "pl"
+     * - Finnish: "fi"
+     * - Dutch: "nl"
+     * - French: "fr"
+     * - Bulgarian: "bg"
+     * - Swedish: "sv" (or "se")
+     * - Chinese Traditional: "zh_tw"
+     * - Chinese Simplified: "zh" (or "zh_cn")
+     * - Turkish: "tr"
+     * - Croatian: "hr"
+     * - Catalan: "ca"
+     *
+     * @param string $lang
+     * @return $this
+     * @see http://openweathermap.org/current#multi
+     */
+    public function setLanguage($lang)
+    {
+        $this->parameters['lang'] = $lang;
 
-		if (isset($this->responseArray['response']) && isset($this->responseArray['response']['error']))
-		{
-			throw new \ErrorException('The Open Weather Map API responded with errors: ' . var_export($this->responseArray['response']['error'], true));
-		} // if
+        return $this;
+    }
 
-		return $this;
-	} // function
+    /**
+     * Method for setting a single parameter.
+     *
+     * @param  string $key
+     * @param  string|int $value
+     * @return $this
+     */
+    public function setParameter($key, $value)
+    {
+        $this->parameters[$key] = $value;
 
+        return $this;
+    }
 
-	/**
-	 * Method for returning the raw response.
-	 *
-	 * @return  string
-	 */
-	public function getResponseJSON ()
-	{
-		return $this->responseJSON;
-	} // function
+    /**
+     * API fetch method.
+     *
+     * @param array $query
+     * @return $this
+     * @throws \ErrorException
+     */
+    public function fetch(array $query = [])
+    {
+        $this->responseJSON = file_get_contents(self::URL . $this->api . '?' . http_build_query(($this->parameters + $query), null, '&'));
+        $this->responseArray = json_decode($this->responseJSON, true);
 
+        if (!is_array($this->responseArray)) {
+            throw new \ErrorException('The Open Weather Map API response returned no valid JSON: ' . $this->responseJSON);
+        }
 
-	/**
-	 * Method for returning the response array.
-	 *
-	 * @return array
-	 */
-	public function getResponseArray ()
-	{
-		return $this->responseArray;
-	} // function
+        return $this;
+    }
 
+    /**
+     * Method for returning the raw response.
+     *
+     * @return  string
+     */
+    public function getResponseJSON()
+    {
+        return $this->responseJSON;
+    }
 
-	/**
-	 * Method for returning the response as object.
-	 *
-	 * @return \stdClass
-	 */
-	public function getResponseObject ()
-	{
-		return json_decode($this->responseJSON);
-	} // function
-} // class
+    /**
+     * Method for returning the response array.
+     *
+     * @return array
+     */
+    public function getResponseArray()
+    {
+        return $this->responseArray;
+    }
+
+    /**
+     * Method for returning the response as object.
+     *
+     * @return \stdClass
+     */
+    public function getResponseObject()
+    {
+        return json_decode($this->responseJSON);
+    }
+}
