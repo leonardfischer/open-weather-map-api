e077f21596e7ada84da1120fdf347894ed01d93b
dev 0815 <post@leonardfischer.de>
2021-01-23 16:58:51 +0100

    Updating request adapter interface and removing old Request.php

--- src/RequestAdapter/RequestAdapterInterface.php
+++ src/RequestAdapter/RequestAdapterInterface.php
@@ -2,6 +2,12 @@
 
 namespace lfischer\openWeatherMap\RequestAdapter;
 
+/**
+ * Interface RequestAdapterInterface
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\RequestAdapter
+ */
 interface RequestAdapterInterface
 {
     public function request(string $url): string;
