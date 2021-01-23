4fb1bd86b5eb8e9aef1211820c8912d226230fc3
dev 0815 <post@leonardfischer.de>
2021-01-23 17:02:53 +0100

    Adding parameter traits and value classes

---
+++ src/Parameter/Language.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace lfischer\openWeatherMap\Parameter;
+
+/**
+ * Class Language
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\Parameter
+ */
+class Language
+{
+    public const AFRIKAANS = 'af';
+
+    public const ALBANIAN = 'al';
+
+    public const ARABIC = 'ar';
+
+    public const AZERBAIJANI = 'az';
+
+    public const BULGARIAN = 'bg';
+
+    public const CATALAN = 'ca';
+
+    public const CZECH = 'cz';
+
+    public const DANISH = 'da';
+
+    public const GERMAN = 'de';
+
+    public const GREEK = 'el';
+
+    public const ENGLISH = 'en';
+
+    public const BASQUE = 'eu';
+
+    public const PERSIAN_FARSI = 'fa';
+
+    public const FINNISH = 'fi';
+
+    public const FRENCH = 'fr';
+
+    public const GALICIAN = 'gl';
+
+    public const HEBREW = 'he';
+
+    public const HINDI = 'hi';
+
+    public const CROATIAN = 'hr';
+
+    public const HUNGARIAN = 'hu';
+
+    public const INDONESIAN = 'id';
+
+    public const ITALIAN = 'it';
+
+    public const JAPANESE = 'ja';
+
+    public const KOREAN = 'kr';
+
+    public const LATVIAN = 'la';
+
+    public const LITHUANIAN = 'lt';
+
+    public const MACEDONIAN = 'mk';
+
+    public const NORWEGIAN = 'no';
+
+    public const DUTCH = 'nl';
+
+    public const POLISH = 'pl';
+
+    public const PORTUGUESE = 'pt';
+
+    public const PORTUGUES_BRASIL = 'pt_br';
+
+    public const ROMANIAN = 'ro';
+
+    public const RUSSIAN = 'ru';
+
+    public const SWEDISH = 'sv';
+
+    public const SWEDISH_SE = 'se';
+
+    public const SLOVAK = 'sk';
+
+    public const SLOVENIAN = 'sl';
+
+    public const SPANISH = 'sp';
+
+    public const SPANISH_ES = 'es';
+
+    public const SERBIAN = 'sr';
+
+    public const THAI = 'th';
+
+    public const TURKISH = 'tr';
+
+    public const UKRAINIAN = 'ua';
+
+    public const UKRAINIAN_UK = 'uk';
+
+    public const VIETNAMESE = 'vi';
+
+    public const CHINESE_SIMPLIFIED = 'zh_cn';
+
+    public const CHINESE_TRADITIONAL = 'zh_tw';
+
+    public const ZULU = 'zu';
+
+    /**
+     * @return string[]
+     */
+    public static function getAll(): array
+    {
+        return [
+            self::AFRIKAANS,
+            self::ALBANIAN,
+            self::ARABIC,
+            self::AZERBAIJANI,
+            self::BULGARIAN,
+            self::CATALAN,
+            self::CZECH,
+            self::DANISH,
+            self::GERMAN,
+            self::GREEK,
+            self::ENGLISH,
+            self::BASQUE,
+            self::PERSIAN_FARSI,
+            self::FINNISH,
+            self::FRENCH,
+            self::GALICIAN,
+            self::HEBREW,
+            self::HINDI,
+            self::CROATIAN,
+            self::HUNGARIAN,
+            self::INDONESIAN,
+            self::ITALIAN,
+            self::JAPANESE,
+            self::KOREAN,
+            self::LATVIAN,
+            self::LITHUANIAN,
+            self::MACEDONIAN,
+            self::NORWEGIAN,
+            self::DUTCH,
+            self::POLISH,
+            self::PORTUGUESE,
+            self::PORTUGUES_BRASIL,
+            self::ROMANIAN,
+            self::RUSSIAN,
+            self::SWEDISH,
+            self::SWEDISH_SE,
+            self::SLOVAK,
+            self::SLOVENIAN,
+            self::SPANISH,
+            self::SPANISH_ES,
+            self::SERBIAN,
+            self::THAI,
+            self::TURKISH,
+            self::UKRAINIAN,
+            self::UKRAINIAN_UK,
+            self::VIETNAMESE,
+            self::CHINESE_SIMPLIFIED,
+            self::CHINESE_TRADITIONAL,
+            self::ZULU
+        ];
+    }
+}
---
+++ src/Parameter/ModeTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace lfischer\openWeatherMap\Parameter;
+
+use InvalidArgumentException;
+
+/**
+ * Trait ModeTrait
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\Parameter
+ */
+trait ModeTrait
+{
+    /**
+     * @var string|null
+     */
+    private $mode = null;
+
+    /**
+     * Method to set the mode.
+     *
+     * @param string|null $mode
+     * @return $this
+     */
+    public function setMode(?string $mode): self
+    {
+        $available = Mode::getAll();
+
+        if (!in_array($mode, $available, true))
+        {
+            throw new InvalidArgumentException(sprintf('The provided mode "%s" is not available, choose on of %s. Or none (=null) for JSON.', $mode, implode(', ', $available)));
+        }
+
+        $this->mode = $mode;
+
+        return $this;
+    }
+
+    /**
+     * Method to get the mode parameter.
+     *
+     * @return array
+     */
+    protected function getModeParameter(): array
+    {
+        return ['mode' => $this->mode];
+    }
+}
