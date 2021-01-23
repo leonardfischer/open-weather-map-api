51a07ca45f2ffd79ad521502a68c2fc0a6754e27
dev 0815 <post@leonardfischer.de>
2021-01-23 17:04:20 +0100

    Add logic specific exceptions

---
+++ src/Exception/SimpleRequestException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lfischer\openWeatherMap\Exception;
+
+use Exception;
+
+/**
+ * Class SimpleRequestException
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\Exception
+ */
+class SimpleRequestException extends Exception
+{
+
+}
---
+++ src/Exception/XmlLoadException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lfischer\openWeatherMap\Exception;
+
+use Exception;
+
+/**
+ * Class XmlLoadException
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\Exception
+ */
+class XmlLoadException extends Exception
+{
+
+}
