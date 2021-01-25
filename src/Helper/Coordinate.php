3466765d92dcab347d42a80d17dd3850c5197d01
dev 0815 <post@leonardfischer.de>
2021-01-25 09:11:46 +0100

    Changing line-feed to LF and making use of CS-Fixer

--- src/Helper/Coordinate.php
+++ src/Helper/Coordinate.php
@@ -4,17 +4,29 @@ namespace lfischer\openWeatherMap\Helper;
 
 use InvalidArgumentException;
 
+/**
+ * Class Coordinate
+ *
+ * @author  Leonard Fischer <post@leonard.fischer.de>
+ * @package lfischer\openWeatherMap\Helper
+ */
 class Coordinate
 {
-    public static function validate(float $latitude, float $longitude)
+    /**
+     * Validate a GPS coordinate.
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public static function validate(float $latitude, float $longitude): void
     {
-        if ($latitude < -90 || $latitude > 90)
-        {
+        if ($latitude < -90 || $latitude > 90) {
             throw new InvalidArgumentException(sprintf('The latitude can only contain values between -90 and +90, you passed %s.', $latitude));
         }
 
-        if ($longitude < -180 || $longitude > 180)
-        {
+        if ($longitude < -180 || $longitude > 180) {
             throw new InvalidArgumentException(sprintf('The longitude can only contain values between -180 and +180, you passed %s.', $latitude));
         }
     }
